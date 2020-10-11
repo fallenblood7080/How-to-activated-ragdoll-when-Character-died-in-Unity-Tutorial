06ff6aa89e149aaa441c7b7cd79cac14727b72c1
fallenblood7080 <67326508+fallenblood7080@users.noreply.github.com>
2020-10-11 20:07:02 +0530

    Add files via upload

---
+++ shoot.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shoot : MonoBehaviour
+{
+    public Camera cam;
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(cam.transform.position, cam.transform.forward, out hit))
+        {
+            Debug.Log($"we hit + {hit.transform.name}");
+            ragdoll doRagdoll = hit.transform.GetComponent<ragdoll>();
+
+            if (doRagdoll != null)
+            {
+                Debug.Log("hit!");
+                doRagdoll.isRagdoll(true);
+                if(hit.rigidbody != null)
+                {
+                    hit.rigidbody.AddForce(-hit.normal * 100f);
+                }
+            }
+        }
+    }
+}
